84db8b83f36376bf5d6c50ec10abb70daee2b1eb
Erick Prado <erickjean36@gmail.com>
2025-11-20 23:17:11 -0400

    update

--- api-nest/src/auth/auth.controller.ts
+++ api-nest/src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiTags } from '@nestjs/swagger';
+import { LoginDto } from './dto/login.dto';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private auth: AuthService) {}
 
   @Post('login')
-  login(@Body() body: { email: string; password: string }) {
-    return this.auth.validate(body.email, body.password);
+  login(@Body() dto: LoginDto) {
+    return this.auth.validate(dto.email, dto.password);
   }
 }
