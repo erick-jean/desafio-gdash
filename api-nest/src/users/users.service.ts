d2e551b0fb4fb7204fde95187456891d20fd643a
Erick Prado <erickjean36@gmail.com>
2025-11-21 14:52:03 -0400

    feat: implement user schema and service with CRUD operations

--- api-nest/src/users/users.service.ts
+++ api-nest/src/users/users.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(data: Partial<User>) {
+    const created = new this.userModel(data);
+    return created.save();
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return this.userModel.find().exec();
   }
 
   findOne(id: number) {
